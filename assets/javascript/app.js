0e5e3803fddeead9df6a9c4aedc98b89165c87b6
unknown <bpham2002@gmail.com>
2018-02-23 04:08:18 -0800

    thirst commit: added check box, check answer, and reorganize cod

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,87 +2,121 @@
      var triviaQuest = [{
          question: 'Leopards are a part of the biological family of _____',
          answers: ["Cats", "Dogs", "Lions", "Tigers"],
-         correctAns: 0
+         correctAns: "Cats"
      }, {
          question: "An owl's eyes are bigger than its brain",
          answers: ["True", "False"],
-         correctAns: 0
+         correctAns: "True"
      }, {
          question: "Bulls charge when they see the color red",
          answers: ["True", "False"],
-         correctAns: 1
+         correctAns: "False"
      }, {
          question: "Sakura' trees that bloom pinkish flowers is also called 'pomegranate blossom'",
          answers: ["True", "False"],
-         correctAns: 1
+         correctAns: "False"
      }, {
          question: "Elephants can be pregnant for two years",
          answers: ["True", "False"],
-         correctAns: 0
+         correctAns: "True"
      }]
      var numQuest = 0
      var correct = 0
      var uncorrect = 0
+     var selectedAns
+     var isAnswerSelected = false;
+     var time = 30
      $("#start").on("click", function() {
-         $(document).on("click", "#selected-answer", function() {
-             correct = checkAns($(this).attr("value"), correct, uncorrect)
-             console.log(correct)
-         })
-         disQuest(numQuest)
 
-
-         function disQuest(num) {
-             $("#main").empty()
-
-             var quest = $("<h2 id='question'>'" + triviaQuest[num].question + "'</h2>")
-             var list = $("<ul id = 'answer-list' type ='A'></ul>")
-             $("#main").append(quest, list)
-             for (var i = 0; i < triviaQuest[num].answers.length; i++) {
-                 $("#answer-list").append($("<li  id = 'selected-answer' value ='" + i + "'>'" + triviaQuest[num].answers[i] + "'</li>"))
-
-             }
-             $("li").hover(function() {
-                 $(this).addClass("blue")
-             }, function() {
-                 $(this).removeClass("blue")
-             })
-             var time = 30
-             $("#question").before($("<p><b>Time remain: </b><span id= 'time-remain'></span></p>"))
-             countDown(time)
-             if (numQuest < triviaQuest.length) {
-                 numQuest++
-             }
+         disQuest(numQuest);
+     });
+     $(document).on("click", "#selected-answer", function() {
+         //  selectedAns = triviaQuest[numQuest].answers[$(this).val()];
+         isAnswerSelected = true;
+         for (var i = 0; i < triviaQuest[numQuest].answers.length; i++) {
+             document.getElementById(i).checked = false;
          }
+         var value = $(this).val();
 
-         function checkAns(ans, cor, uncor) {
-             if (parseInt(ans) === triviaQuest[num].correctAns) {
-                 alert("correct")
-                 cor++
-             } else {
-                 alert("uncorrect")
-                 uncor++
-             }
-
-
-             return cor
-
-         }
-
-         function countDown(t) {
-
-             var count = setInterval(function() {
-
-                 $("#time-remain").text(t-- + " seconds")
-             }, 1000)
-             setTimeout(function() {
-                 disQuest(numQuest)
-                 clearInterval(count)
-             }, 31000)
-
-
-         };
+         document.getElementById(value).checked = true;
+         selectedAns = $(this).text();
 
      })
+
+     function disQuest(num) {
+
+         $("#main").empty()
+
+         var quest = $("<h2 id='question'></h2> ")
+         quest.append(triviaQuest[num].question);
+         var ulist = $("<ul id = 'answer-list' type ='circle'></ul>")
+         $("#main").append(quest, ulist);
+         for (var i = 0; i < triviaQuest[num].answers.length; i++) {
+             var list = $("<li  id = 'selected-answer' value = '" + i + "'></li>")
+             list.append($("<input type = 'checkbox' class = 'check-box' id = '" + i + "'>"))
+             list.append(triviaQuest[num].answers[i])
+             ulist.append(list)
+
+         }
+         $("li").hover(function() {
+             $(this).addClass("blue")
+         }, function() {
+             $(this).removeClass("blue")
+         });
+         countDown(time);
+
+     }
+
+     function checkAns(ans) {
+
+
+         if (ans === triviaQuest[numQuest].correctAns) {
+             alert("correct")
+             correct++
+
+         } else {
+             alert("uncorrect")
+             uncorrect++
+
+         }
+
+
+     }
+
+     function countDown(t) {
+
+         $("#question").before($("<p><b>Time remain: </b><span id= 'time-remain'></span></p>"));
+
+         var count = setInterval(function() {
+
+             $("#time-remain").text(t-- + " seconds")
+         }, 1000)
+         setTimeout(function() {
+             if (isAnswerSelected) {
+
+                 checkAns(selectedAns);
+
+
+             } else {
+                 uncorrect++
+                 selectedAns = null
+                 alert("unselected")
+             }
+             console.log(selectedAns)
+             console.log("correct: " + correct)
+             console.log("uncorrect: " + uncorrect)
+             if (numQuest < triviaQuest.length - 1) {
+                 numQuest++
+                 disQuest(numQuest)
+             }
+             isAnswerSelected = false;
+             clearInterval(count)
+         }, 31000)
+
+
+     };
+
+
 
 
 
